1c744c2c12042ad0213d49a07d389ae3b38494ee
felix <felixletsplay@outlook.de>
2018-08-16 21:46:46 +0200

    (started with js (not really)) client should not cache now refresh time in footer

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import make_response
 from ConnectionManager import CommitConnection
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,12 +10,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main_page():
     # TODO get users dynamicaly
-    year = datetime.datetime.now().strftime("%Y")
+    year = datetime.now().strftime("%Y")
     users = list()
     users.append(
         {"name": "felixletsplayyt", "contributions": CommitConnection.getCommitsInYear(year, "felixletsplayyt")})
     users.append({"name": "robmroi03", "contributions": CommitConnection.getCommitsInYear(year, "robmroi03")})
-    return render_template("index.html.twig", users=users)
+    resp = make_response(render_template("index.html.twig", users=users, time=datetime.now().strftime('%H:%M:%S')))
+    resp.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    resp.headers['Pragma'] = "no-cache"
+    resp.headers['Expires'] = "0"
+    return resp
 
 
 if __name__ == '__main__':
---
+++ static/index.js
@@ -0,0 +1,3 @@
+function onLoad(){
+    document.getElementById("")
+}
