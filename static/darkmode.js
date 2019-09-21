f9cb408a66dd0574908ba664660e783bf813f556
Felix <felixletsplay@outlook.de>
2019-09-21 10:29:34 +0200

    Add extra dark mode options: !to invert color and ? to set a class

--- static/darkmode.js
+++ static/darkmode.js
@@ -13,35 +13,51 @@ function changeDarkMode(userSet) {
             button.style.color = "#f5f5f5";
         }
     }
-    let newColor;
+    let themeColor;
+    let oppositeColor;
     if (document.darkMode === true) {
         document.darkMode = false;
         if (userSet) {
             localStorage.setItem("dark", "false");
         }
         document.body.style.backgroundColor = "#f5f5f5";
-        newColor = '#1D1F21';
+        themeColor = '#1D1F21';
+        oppositeColor = '#f5f5f5';
     } else {
         document.darkMode = true;
         if (userSet) {
             localStorage.setItem("dark", "true");
         }
         document.body.style.backgroundColor = "#1D1F21";
-        newColor = '#f5f5f5';
+        themeColor = '#f5f5f5';
+        oppositeColor = '#1D1F21';
     }
     $("[data-affect-dark-mode]").each(function () {
         let attribute = this.getAttribute("data-affect-dark-mode");
         if (attribute.includes("&")) {
             attribute.split("&").forEach((attr) => {
-                this.style[attr] = newColor;
+                setModeForElement(this, attr, themeColor, oppositeColor, document.darkMode);
             });
         } else {
-            this.style[attribute] = newColor;
+            setModeForElement(this, attribute, themeColor, oppositeColor, document.darkMode);
         }
     });
 
 }
 
+function setModeForElement(element, option, themeColor, oppositeColor, isDark) {
+    if (option.startsWith('!')) {
+        element.style[option.slice(1)] = oppositeColor;
+    } else if (option.startsWith('?')) {
+        if (isDark) {
+            element.classList.add(option.slice(1))
+        } else {
+            element.classList.remove(option.slice(1))
+        }
+    } else {
+        element.style[option] = themeColor;
+    }
+}
 
 if (localStorage.getItem("dark") === "true") {
     changeDarkMode(false)
